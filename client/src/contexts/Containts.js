9c9e82a1f616036f0df9fe202fb24405fb34f2ae
hovanhung1506 <hungho269@gmail.com>
2022-04-10 00:14:02 +0700

    10-04-22

--- client/src/contexts/Containts.js
+++ client/src/contexts/Containts.js
@@ -1,4 +1,5 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'somedeployedurl';
+export const apiUrl =
+  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://fierce-sands-91695.herokuapp.com/api';
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
 export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS';
 export const POSTS_LOADED_FAIL = 'POSTS_LOADED_FAIL';
